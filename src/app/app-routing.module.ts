6981b15c722660f65b52191ce61d79533c53cce0
barbarasalla <barbara.msalla@gmail.com>
2022-07-06 06:10:18 -0300

    adicionando opção apagar categoria

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { CategoriaComponent } from './categoria/categoria.component';
+import { DeleteCategoriaComponent } from './delete/delete-categoria/delete-categoria.component';
 import { EditCategoriaComponent } from './edit/edit-categoria/edit-categoria.component';
 import { EntrarComponent } from './entrar/entrar.component';
 
@@ -13,7 +14,8 @@ const routes: Routes = [
 {path: 'entrar', component: EntrarComponent},
 {path: 'cadastrar', component: CadastrarComponent},
 {path: 'categoria', component: CategoriaComponent},
-{path: 'edit-categoria/:id', component: EditCategoriaComponent}
+{path: 'edit-categoria/:id', component: EditCategoriaComponent},
+{path: 'delete-categoria/:id', component: DeleteCategoriaComponent}
 ];
 
 @NgModule({
